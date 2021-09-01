9273eb36b49e7638b1f139b03701c03ee218fb85
Shengyou Fan <shengyoufan@gmail.com>
2021-09-01 19:19:32 +0800

    add sub function

--- src/main/kotlin/io/kraftsman/Calculator.kt
+++ src/main/kotlin/io/kraftsman/Calculator.kt
@@ -8,4 +8,12 @@ class Calculator {
     fun add(x: Double, y: Double): Double {
         return (x + y)
     }
+
+    fun sub(x: Int, y: Int): Double {
+        return (x - y).toDouble()
+    }
+
+    fun sub(x: Double, y: Double): Double {
+        return x - y
+    }
 }
--- src/test/kotlin/io/kraftsman/CalculatorTest.kt
+++ src/test/kotlin/io/kraftsman/CalculatorTest.kt
@@ -7,7 +7,7 @@ class CalculatorTest : FunSpec({
 
     context("A Calculator") {
 
-        context("Can compatible with Int and Double when calculate") {
+        context("Can compatible with Int and Double when add") {
             test("Should return 2.0 when 1 + 1") {
                 val calculator = Calculator()
                 calculator.add(1, 1) shouldBe 2.0
@@ -15,6 +15,17 @@ class CalculatorTest : FunSpec({
             test("Should return 4.0 when 2.0 + 2.0") {
                 val calculator = Calculator()
                 calculator.add(2.0, 2.0) shouldBe 4.0
+            }
+        }
+
+        context("Can compatible with Int and Double when sub") {
+            test("Should return 1.0 when 2 - 1") {
+                val calculator = Calculator()
+                calculator.sub(2, 1) shouldBe 1.0
+            }
+            test("Should return 0.0 when 2.0 - 2.0") {
+                val calculator = Calculator()
+                calculator.sub(2.0, 2.0) shouldBe 0.0
             }
         }
     }

de2859b94286c5c635623d17caf9c049594fb787
Shengyou Fan <shengyoufan@gmail.com>
2021-09-01 23:45:06 +0800

    add div function

--- src/main/kotlin/io/kraftsman/Calculator.kt
+++ src/main/kotlin/io/kraftsman/Calculator.kt
@@ -8,4 +8,12 @@ class Calculator {
     fun add(x: Double, y: Double): Double {
         return (x + y)
     }
+
+    fun div(x: Int, y: Int): Double {
+        return (x / y).toDouble()
+    }
+
+    fun div(x: Double, y: Double): Double {
+        return x / y
+    }
 }
--- src/test/kotlin/io/kraftsman/CalculatorTest.kt
+++ src/test/kotlin/io/kraftsman/CalculatorTest.kt
@@ -7,7 +7,7 @@ class CalculatorTest : FunSpec({
 
     context("A Calculator") {
 
-        context("Can compatible with Int and Double when calculate") {
+        context("Can compatible with Int and Double when add") {
             test("Should return 2.0 when 1 + 1") {
                 val calculator = Calculator()
                 calculator.add(1, 1) shouldBe 2.0
@@ -15,6 +15,17 @@ class CalculatorTest : FunSpec({
             test("Should return 4.0 when 2.0 + 2.0") {
                 val calculator = Calculator()
                 calculator.add(2.0, 2.0) shouldBe 4.0
+            }
+        }
+
+        context("Can compatible with Int and Double when dev") {
+            test("Should return 3.0 when 6 / 2") {
+                val calculator = Calculator()
+                calculator.div(6, 2) shouldBe 3.0
+            }
+            test("Should return 3.0 when 9.0 + 3.0") {
+                val calculator = Calculator()
+                calculator.div(9.0, 3.0) shouldBe 3.0
             }
         }
     }
